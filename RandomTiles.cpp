121046a75bb305e15693e29e272f47e4816295ac
Maeyanie <me@maeyanie.com>
2024-11-22 05:01:20 -0500

    Add project files.

---
+++ RandomTiles.cpp
@@ -0,0 +1,67 @@
+﻿#include "RandomTiles.h"
+#include <random>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int sizeX = 2;
+	int sizeY = 2;
+	filesystem::path sourcedir = ".";
+	filesystem::path targetfile = sourcedir / "random_tile.stl";
+
+	switch (argc) {
+	case 0:
+	case 1:
+		break;
+	case 2:
+		sizeX = sizeY = atoi(argv[1]);
+		break;
+	case 3:
+		sizeX = atoi(argv[1]);
+		sizeY = atoi(argv[2]);
+		break;
+	case 4:
+		sizeX = atoi(argv[1]);
+		sizeY = atoi(argv[2]);
+		sourcedir = argv[3];
+		targetfile = sourcedir / "random_tile.stl";
+		break;
+	default:
+		sizeX = atoi(argv[1]);
+		sizeY = atoi(argv[2]);
+		sourcedir = argv[3];
+		targetfile = argv[4];
+		break;
+	}
+
+	vector<filesystem::path> sources;
+	for (auto const& i : filesystem::directory_iterator(sourcedir)) {
+		if (i.path().extension() == ".stl") {
+			cout << "Found STL file: " << i << endl;
+			sources.push_back(i);
+		}
+	}
+	cout << "Total sources: " << sources.size() << endl;
+	if (sources.size() == 0) return 1;
+
+	STL target;
+	target.box(sizeX * TILESIZE, sizeY * TILESIZE, 1.0f);
+
+	mt19937 rng(time(NULL));
+	uniform_int_distribution<> rngsource(0, sources.size()-1);
+	uniform_int_distribution<> rngrotate(0, 3);
+	uniform_int_distribution<> rngmirror(0, 1);
+	for (int x = 0; x < sizeX; x++) {
+		for (int y = 0; y < sizeY; y++) {
+			STL source(sources[rngsource(rng)]);
+			source.rotate(rngrotate(rng));
+			if (rngmirror(rng)) source.mirror();
+			source.offset(x * TILESIZE, y * TILESIZE, 0.0f);
+			target.merge(source);
+		}
+	}
+
+	target.write(targetfile);
+	return 0;
+}
---
+++ RandomTiles.h
@@ -0,0 +1,83 @@
+﻿// RandomTiles.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <list>
+#include <filesystem>
+
+#define TILESIZE (25.4f)
+
+struct Point {
+	float x, y, z;
+
+	inline Point() { x = y = z = 0.0f; }
+	inline Point(float X, float Y, float Z) {
+		x = X;
+		y = Y;
+		z = Z;
+	}
+
+	inline Point& operator=(const Point& rhs) {
+		x = rhs.x;
+		y = rhs.y;
+		z = rhs.z;
+		return *this;
+	}
+	friend std::ostream& operator<<(std::ostream& os, const Point& point);
+};
+static inline std::ostream& operator<<(std::ostream& os, const Point& p) {
+	os << p.x << "," << p.y << "," << p.z;
+	return os;
+}
+
+struct Tri {
+	Point p[3];
+
+	inline Tri() {}
+	inline Tri(Point points[3]) {
+		memcpy(p, points, 3 * sizeof(Point));
+	}
+	inline Tri(Point a, Point b, Point c) {
+		p[0] = a;
+		p[1] = b;
+		p[2] = c;
+	}
+	
+	inline Tri& operator=(const Tri& rhs) {
+		memcpy(p, rhs.p, 3 * sizeof(Point));
+		return *this;
+	}
+};
+
+class STL {
+public:
+	std::list<Tri> mesh;
+
+	STL() {}
+	STL(std::filesystem::path file);
+	STL(STL& rhs) {
+		mesh = rhs.mesh;
+	}
+
+	STL& operator=(STL& rhs) {
+		mesh = rhs.mesh;
+		return *this;
+	}
+
+	void write(std::filesystem::path file);
+
+	void box(float sizeX, float sizeY, float sizeZ);
+	void offset(float x, float y, float z);
+	void rotate();
+	void rotate(int times);
+	void mirror();
+	void merge(STL& stl);
+	void merge(STL* stl);
+};
+
+
